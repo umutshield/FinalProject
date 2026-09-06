fbfeabd2074fe9048351bc4aab343a968d0bba27
Ümit Kalkan <umutshield03@gmail.com>
2026-09-06 22:54:02 +0300

    d11-http/get/post

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -28,16 +28,45 @@ namespace WebAPI.Controllers
             _productService = productService;
         }
 
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
             /* Dependecy chain -- bağımlılık zinciri 
             kodumuzu ProductManager'a yani IProductDal'a yani EfProductDal'a yani entityframework'e bağımlı hale getirtmek istemiyoruz 
             bu yüzden code refactoring yapacağız. eski kod:  IProductService productService = new ProductManager(new EfProductDal()); */
+            //Swagger -- API Documentation
 
             var result = _productService.GetAll();
-            return result.Data;
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
 
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        /* Get, kullanıcının api'dan istediği datadır. Post ise kullanıcının api'ya yolladığı bir datadır. 
+        örn instagram'a post attım diyoruz oradaki post bizim yolladığımız bir datadır. */
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);     //ürün eklemek için: postman>post>body>raw>json formatında ekle.
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
